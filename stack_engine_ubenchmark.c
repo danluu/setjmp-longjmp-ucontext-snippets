56b9d63cb01cb858275436d3a2f4ab442d911425
Marek Majkowski <majek04@gmail.com>
2013-03-18 03:24:44 +0000

    And even more abstraction - put tests in the loop

--- stack_engine_ubenchmark.c
+++ stack_engine_ubenchmark.c
@@ -54,25 +54,40 @@ static uint64_t test_rwdep() {
 }
 
 int main() {
-	int j;
-	uint64_t a = 0, b = 0, c = 0, t;
+	struct {
+		uint64_t (*fun)();
+		char *name;
+		uint64_t t;
+	} tests[] = {
+		{test_nodep, "no dependency", 0},
+		{test_rodep, "read dependency", 0},
+		{test_rwdep, "read+write dependency", 0},
+		{NULL, NULL, 0}
+	};
 
-	for (j = 0; j < RUNS; ++j) {
-		t = test_nodep();
-		a += t / RUNS;
-		printf("A: %llu\n", t);
-
-		t = test_rodep();
-		b += t / RUNS;
-		printf("B: %llu\n", t);
-
-		t = test_rwdep();
-		c += t / RUNS;
-		printf("C: %llu\n", t);
+	printf("[*] Warming up the code\n");
+	int test_no, run_no;
+	for (run_no = 0; run_no < 3; run_no += 1) {
+		for (test_no = 0; tests[test_no].fun; test_no += 1) {
+			tests[test_no].fun();
+		}
 	}
 
-	printf("no dependency / read dependency / read+write dependency: "
-	       "%llu %llu %llu\n", a, b, c);
+	printf("[*] Running the benchmarks\n");
+	for (run_no = 0; run_no < RUNS; run_no += 1) {
+		for (test_no = 0; tests[test_no].fun; test_no += 1) {
+			uint64_t t = tests[test_no].fun();
+			tests[test_no].t += t;
+			printf("[ ] #%i %llu\n", test_no, t);
+		}
+	}
+
+	for (test_no = 0; tests[test_no].fun; test_no += 1) {
+		printf("[=] %-25s %llu\n",
+		       tests[test_no].name,
+		       tests[test_no].t
+			);
+	}
 
 	return 0;
 }
