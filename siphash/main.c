342fdac8b967d11363986e4e131b77dcb05f7536
Marek Majkowski <majek04@gmail.com>
2013-03-22 06:21:51 +0000

    siphash assembly

--- siphash/main.c
+++ siphash/main.c
@@ -7,6 +7,7 @@
 #include <sys/resource.h>
 
 #include "rdtsc.h"
+#include "stddev.h"
 
 void siphash24_half_round_asm(const uint64_t in[4], uint64_t out[4]);
 
@@ -16,6 +17,7 @@ void half_round(const uint64_t in[4], uint64_t out[4]);
 #define ROUNDS 30
 
 struct {
+	int noop;
 	int d0;
 	int d1;
 } rounds[ROUNDS];
@@ -25,11 +27,9 @@ int main() {
 	uint64_t in[4] = {1, 2, 3, 4};
 	uint64_t out1[4], out2[4];
 
-	/* Use 100% CPU for some time */
-	for (i = 0; i < 10000000; i++) {
-		half_round(in, out1);
-		siphash24_half_round_asm(in, out2);
-	}
+	/* sanity check */
+	half_round(in, out1);
+	siphash24_half_round_asm(in, out2);
 
 	printf("lo=%016llx hi=%016llx\n", out1[0], out1[1]);
 	printf("lo=%016llx hi=%016llx\n", out1[2], out1[3]);
@@ -40,6 +40,15 @@ int main() {
 		assert(out1[i] == out2[i]);
 	}
 
+
+	/* Use 100% CPU for some time to make sure the processor has
+	   time to turn off energy saving. */
+	for (i = 0; i < 50000000; i++) {
+		half_round(in, out1);
+		siphash24_half_round_asm(in, out1);
+	}
+
+	/* The hot section */
 	struct rusage rus_before;
 	getrusage(RUSAGE_SELF, &rus_before);
 
@@ -51,16 +60,31 @@ int main() {
 	siphash24_half_round_asm(in, out1);
 	half_round(in, out1);
 
+	struct stddev noop = INIT_STDDEV;
+	struct stddev native = INIT_STDDEV;
+	struct stddev sse = INIT_STDDEV;
+
 	for (i = 0; i < ROUNDS; i++) {
-		uint64_t c0, c1, c2, c3;
-		RDTSC_START(c2);
-		siphash24_half_round_asm(in, out1);
-		RDTSC_STOP(c3);
+		uint64_t c0, c1;
+		RDTSC_START(c0);
+		RDTSC_STOP(c1);
+		stddev_add(&noop, c1 - c0);
+
 		RDTSC_START(c0);
 		half_round(in, out1);
+		half_round(in, out1);
+		half_round(in, out1);
+		half_round(in, out1);
 		RDTSC_STOP(c1);
-		rounds[i].d0 = c1 - c0;
-		rounds[i].d1 = c3 - c2;
+		stddev_add(&native, c1 - c0);
+
+		RDTSC_START(c0);
+		siphash24_half_round_asm(in, out1);
+		siphash24_half_round_asm(in, out1);
+		siphash24_half_round_asm(in, out1);
+		siphash24_half_round_asm(in, out1);
+		RDTSC_STOP(c1);
+		stddev_add(&sse, c1 - c0);
 	}
 
 	struct rusage rus_after;
@@ -82,14 +106,21 @@ int main() {
 	i = rus_after.ru_nivcsw - rus_before.ru_nivcsw;
 	if (i) printf("nivcsw=%i\n", i);
 
-	unsigned int d0 = -1, d1 = -1;
-	for (i = 0; i < ROUNDS; i++) {
-		if (d0 > rounds[i].d0)
-			d0 = rounds[i].d0;
-		if (d1 > rounds[i].d1)
-			d1 = rounds[i].d1;
-		printf("    C =%4i  ASM =%4i\n", rounds[i].d0, rounds[i].d1);
-	}
-	printf("MIN C=%4i ASM=%4i   diff=%.3f%%\n", d0, d1, (d0-d1)*100./d0);
+	printf("reporting in cycles:\n");
+
+	int noop_min, min, max;
+	double avg, dev, a, b;
+	stddev_get(&noop, NULL, &noop_min, &max, &avg, &dev);
+	printf("noop round   min/max=%i/%i   avg=%.3f  +/- %.3f\n",
+	       noop_min, max, avg, dev);
+	stddev_get(&native, NULL, &min, &max, &avg, &dev);
+	printf("native       min/max=%i/%i   avg=%.3f  +/- %.3f\n",
+	       min-noop_min, max-noop_min, avg-noop_min, dev);
+	a = min-noop_min;
+	stddev_get(&sse, NULL, &min, &max, &avg, &dev);
+	printf("sse          min/max=%i/%i   avg=%.3f  +/- %.3f\n",
+	       min-noop_min, max-noop_min, avg-noop_min, dev);
+	b = min-noop_min;
+	printf("gain: %.3f%%\n", (1. - b/a)*100.);
 	return 0;
 }
