5c432e5314c358c85361575c6937221a02f616b5
Marek Majkowski <majek04@gmail.com>
2013-03-24 03:38:49 +0000

    minor reorganizations

--- siphash/main.c
+++ siphash/main.c
@@ -15,12 +15,35 @@ uint64_t siphash24_asm(const void *src, unsigned long src_sz, const char key[16]
 
 #define ROUNDS 60
 #define MAX_RETRIES 100
+#define ACCURACY 0.04
 
-static int do_test(char *vector, int vector_sz, char key[16]) {
+static int do_noop_min() {
+	int retry;
+	for (retry = 0; retry < MAX_RETRIES; retry += 1) {
+		struct stddev noop = INIT_STDDEV;
+		int i;
+		for (i = 0; i < ROUNDS*10; i++) {
+			uint64_t c0, c1;
+			RDTSC_START(c0);
+			RDTSC_STOP(c1);
+			stddev_add(&noop, c1 - c0);
+		}
+		int noop_min;
+		double avg_noop, dev_noop;
+		stddev_get(&noop, NULL, &noop_min, NULL, &avg_noop, &dev_noop);
+		if (dev_noop > ACCURACY * avg_noop)
+			continue;
+		return noop_min;
+	}
+	printf("FATAL! Can't measure NOOP with decent accuracy\n");
+	exit(1);
+}
+
+
+static int do_test(char *vector, int vector_sz, char key[16], int noop_min) {
 	uint64_t out = 1;
 	out = out;
 	struct {
-		int noop;
 		int d0;
 		int d1;
 	} rounds[ROUNDS];
@@ -34,17 +57,12 @@ static int do_test(char *vector, int vector_sz, char key[16]) {
 	out = siphash24(vector, vector_sz, key);
 	out = siphash24_asm(vector, vector_sz, key);
 
-	struct stddev noop = INIT_STDDEV;
 	struct stddev native = INIT_STDDEV;
 	struct stddev sse = INIT_STDDEV;
 
 	int i;
 	for (i = 0; i < ROUNDS; i++) {
 		uint64_t c0, c1;
-		RDTSC_START(c0);
-		RDTSC_STOP(c1);
-		stddev_add(&noop, c1 - c0);
-
 		RDTSC_START(c0);
 		out = siphash24(vector, vector_sz, key);
 		RDTSC_STOP(c1);
@@ -68,33 +86,26 @@ static int do_test(char *vector, int vector_sz, char key[16]) {
 	    (rus_after.ru_nivcsw - rus_before.ru_nivcsw))
 		return 0;
 
-	double avg_noop, avg_sse, avg_native,
-		dev_noop, dev_sse, dev_native;
-	stddev_get(&noop, NULL, NULL, NULL, &avg_noop, &dev_noop);
+	double avg_sse, avg_native, dev_sse, dev_native;
 	stddev_get(&native, NULL, NULL, NULL, &avg_native, &dev_native);
 	stddev_get(&sse, NULL, NULL, NULL, &avg_sse, &dev_sse);
-	double fra = 0.04;
-	if ((dev_noop > fra * avg_noop) ||
-	    (dev_native > fra * avg_native) ||
-	    (dev_sse > fra * avg_sse))
+	if ((dev_native > ACCURACY * avg_native) ||
+	    (dev_sse > ACCURACY * avg_sse))
 		return 0;
 
 
-	int noop_min, min, max;
-	double avg, dev, a, b;
-	stddev_get(&noop, NULL, &noop_min, &max, &avg, &dev);
-	printf("noop round   min/max=%3i/%3i   avg=%7.3f  +/- %.3f\n",
-	       noop_min, max, avg, dev);
+	int min, max, a, b;
+	double avg, dev;
 	stddev_get(&native, NULL, &min, &max, &avg, &dev);
-	printf("native       min/max=%3i/%3i   avg=%7.3f  +/- %.3f\n",
-	       min-noop_min, max-noop_min, avg-noop_min, dev);
 	a = min-noop_min;
+	printf("native       min/max=%3i/%3i   avg=%7.3f  +/- %.3f\n",
+	       a, max-noop_min, avg-noop_min, dev);
 	stddev_get(&sse, NULL, &min, &max, &avg, &dev);
-	printf("sse          min/max=%3i/%3i   avg=%7.3f  +/- %.3f\n",
-	       min-noop_min, max-noop_min, avg-noop_min, dev);
 	b = min-noop_min;
+	printf("sse          min/max=%3i/%3i   avg=%7.3f  +/- %.3f\n",
+	       b, max-noop_min, avg-noop_min, dev);
 	printf("gain: %.3f%%   CPB=%.3f  for sz=%i\n",
-	       (1. - b/a)*100., b / vector_sz, vector_sz);
+	       (1. - (double)b/a)*100., (double)b / vector_sz, vector_sz);
 	return 1;
 }
 
@@ -102,8 +113,8 @@ int main() {
 	char key[16] = {0};
 	uint64_t out1, out2;
 
-	/* char *vector = "abcdefghabcdefghabcdefghabcdefgh"; */
-	char *vector = "";
+	char *vector = "abcdefghabcdefghabcdefghabcdefgh";
+	/* char *vector = ""; */
 	int vector_sz = strlen(vector);
 
 	/* sanity check */
@@ -122,14 +133,16 @@ int main() {
 		out1 = siphash24_asm(vector, vector_sz, key);
 	}
 
+	int noop_min = do_noop_min();
+	printf("[+] empty run: %i cycles\n", noop_min);
 	int sz;
-	for (sz = 0; sz <= vector_sz; sz++) {
+	for (sz = 31; sz <= 32; sz++) {
 		out1 = siphash24(vector, sz, key);
 		out2 = siphash24_asm(vector, sz, key);
 		assert(out1 == out2);
 		int retry;
 		for (retry = 0; retry < MAX_RETRIES; retry += 1)
-			if (do_test(vector, sz, key) != 0)
+			if (do_test(vector, sz, key, noop_min) != 0)
 				break;
 		if (retry == MAX_RETRIES) {
 			printf("FAILED!\n");
