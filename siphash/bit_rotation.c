60e95e4e7f425e6b8c1877c7bc6d749dc8b97c15
Marek Majkowski <majek04@gmail.com>
2013-03-26 21:23:13 +0000

    indentation

--- siphash/bit_rotation.c
+++ siphash/bit_rotation.c
@@ -10,69 +10,69 @@ static void bit_rotate(uint8_t input[8*VECTORS], uint64_t output[VECTORS]);
 static void print_matrix(uint64_t data[VECTORS]);
 
 int main() {
-  uint8_t input[8*VECTORS];
-  uint64_t output[VECTORS];
-  uint64_t output2[VECTORS];
+	uint8_t input[8*VECTORS];
+	uint64_t output[VECTORS];
+	uint64_t output2[VECTORS];
 
-  memset(input, 0, sizeof(input));
-  
-  int i, j;
-  for(i = 0; i < VECTORS; i++){
-    input[i*8] = 0x80;
-  }
-  
-  input[0] = 0x0A;
+	memset(input, 0, sizeof(input));
 
-  bit_rotate(input, output);
-  bit_rotate((uint8_t*)output, output2);
+	int i, j;
+	for(i = 0; i < VECTORS; i++){
+		input[i*8] = 0x80;
+	}
 
-  int r = memcmp(input,output2,8*VECTORS);
-  printf("memcmp result: %i\n", r);
+	input[0] = 0x0A;
+
+	bit_rotate(input, output);
+	bit_rotate((uint8_t*)output, output2);
+
+	int r = memcmp(input,output2,8*VECTORS);
+	printf("memcmp result: %i\n", r);
 
 
-  print_matrix((uint64_t*)input);
-  printf("\n");
-  print_matrix(output);
-  printf("\n");
-  print_matrix(output2);
-  return 0;
+	print_matrix((uint64_t*)input);
+	printf("\n");
+	print_matrix(output);
+	printf("\n");
+	print_matrix(output2);
+	return 0;
 }
 
 static void print_matrix(uint64_t data[VECTORS]) {
-  int i, j;
+	int i, j;
 
-  for(i = 0; i < 8; i++) {
-    for(j = 0; j < 8; j++){
-      printf("%016llx ", data[i*8+j]);
-    }
-    printf("\n");
-  }
+	for(i = 0; i < 8; i++) {
+		for(j = 0; j < 8; j++){
+			printf("%016llx ", data[i*8+j]);
+		}
+		printf("\n");
+	}
 }
 
 union xmm {
-  __m128i x;
-  uint8_t b[16];
+	__m128i x;
+	uint8_t b[16];
 };
 
 static void bit_rotate(uint8_t input[8*VECTORS], uint64_t output[VECTORS]) {
-  int i, j, k, b;
+	int i, j, k, b;
 
-  uint16_t* o = (uint16_t*)&output[0];
+	uint16_t* o = (uint16_t*)&output[0];
 
-  for(b = 0; b < 8; b++) {
-    for(i = 0; i < VECTORS/16; i++) {    
-      union xmm x;
-      for(j = 0; j < 16; j++) {
-	int input_num = i*16+j;
-	x.b[j] = input[(input_num)*8+b];
-      }
-      for(k = 7; k >= 0; k--){
-	uint16_t v = _mm_movemask_epi8(x.x);
-	x.x = _mm_slli_epi64(x.x, 1);
-	int bit_num = b*8 + k;
-	o[bit_num*4 + i] = v;
-      }
-    }
-  }
+	for(b = 0; b < 8; b++) {
+		for(i = 0; i < VECTORS/16; i++) {
+			union xmm x;
+			for(j = 0; j < 16; j++) {
+				int input_num = i*16+j;
+				x.b[j] = input[(input_num)*8+b];
+			}
+			for(k = 7; k >= 0; k--){
+				uint16_t v = _mm_movemask_epi8(x.x);
+				x.x = _mm_slli_epi64(x.x, 1);
+				int bit_num = b*8 + k;
+				o[bit_num*4 + i] = v;
+			}
+		}
+	}
 
 }
