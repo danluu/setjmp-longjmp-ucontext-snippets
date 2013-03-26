2e67314c03707c67c9152fd06a6f04f3c4cfbc1d
Marek Majkowski <majek04@gmail.com>
2013-03-26 21:23:56 +0000

    put main at the bottom

--- siphash/bit_rotation.c
+++ siphash/bit_rotation.c
@@ -6,38 +6,6 @@
 
 #define VECTORS 64
 
-static void bit_rotate(uint8_t input[8*VECTORS], uint64_t output[VECTORS]);
-static void print_matrix(uint64_t data[VECTORS]);
-
-int main() {
-	uint8_t input[8*VECTORS];
-	uint64_t output[VECTORS];
-	uint64_t output2[VECTORS];
-
-	memset(input, 0, sizeof(input));
-
-	int i, j;
-	for(i = 0; i < VECTORS; i++){
-		input[i*8] = 0x80;
-	}
-
-	input[0] = 0x0A;
-
-	bit_rotate(input, output);
-	bit_rotate((uint8_t*)output, output2);
-
-	int r = memcmp(input,output2,8*VECTORS);
-	printf("memcmp result: %i\n", r);
-
-
-	print_matrix((uint64_t*)input);
-	printf("\n");
-	print_matrix(output);
-	printf("\n");
-	print_matrix(output2);
-	return 0;
-}
-
 static void print_matrix(uint64_t data[VECTORS]) {
 	int i, j;
 
@@ -75,4 +43,34 @@ static void bit_rotate(uint8_t input[8*VECTORS], uint64_t output[VECTORS]) {
 		}
 	}
 
+}
+
+
+int main() {
+	uint8_t input[8*VECTORS];
+	uint64_t output[VECTORS];
+	uint64_t output2[VECTORS];
+
+	memset(input, 0, sizeof(input));
+
+	int i, j;
+	for(i = 0; i < VECTORS; i++){
+		input[i*8] = 0x80;
+	}
+
+	input[0] = 0x0A;
+
+	bit_rotate(input, output);
+	bit_rotate((uint8_t*)output, output2);
+
+	int r = memcmp(input,output2,8*VECTORS);
+	printf("memcmp result: %i\n", r);
+
+
+	print_matrix((uint64_t*)input);
+	printf("\n");
+	print_matrix(output);
+	printf("\n");
+	print_matrix(output2);
+	return 0;
 }
