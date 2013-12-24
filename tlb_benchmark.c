de7cce32e552f9fb74fc0879d56f0fa6e7df6e89
Dan Luu <danluu@gmail.com>
2013-12-24 16:36:42 -0600

    Refactor into single function

--- tlb_benchmark.c
+++ tlb_benchmark.c
@@ -2,15 +2,14 @@
 #include "rdtsc.h"
 #include "stddev.h"
 
-#define RUNS 100
+#define RUNS 1000
 
 #define NUM_LINES 10000
 #define LINE_SIZE 128
-#define OFFSET  
 #define PG_SIZE 4096
 
-int page_aligned() {
-  static char a[NUM_LINES * PG_SIZE];
+int access_mem(int align) {
+  static char a[2 * NUM_LINES * PG_SIZE];
   int sum = 0;
   uint64_t tsc_before, tsc_after, tsc;
   uint64_t offset;
@@ -20,11 +19,10 @@ int page_aligned() {
     a[i] = i % 17;
   }
 
-
   // Warmup
   offset = 0;
   for (j = 0; j < NUM_LINES; j++) {
-    offset += PG_SIZE;
+    offset += PG_SIZE + align;
     sum += a[offset];
   }
 
@@ -32,42 +30,7 @@ int page_aligned() {
   for (i = 0; i < RUNS; i++) {
     offset = 0;
     for (j = 0; j < NUM_LINES; j++) {
-      offset += PG_SIZE;
-      sum += a[offset];
-    }
-  }
-  RDTSC_START(tsc_after);
-  tsc = tsc_after - tsc_before;
-
-  printf("Total time: %llu \n", tsc);
-
-  return sum;
-}
-
-int unaligned() {
-  static char a[NUM_LINES * PG_SIZE];
-  int sum = 0;
-  uint64_t tsc_before, tsc_after, tsc;
-  uint64_t offset;
-
-  int i, j;
-  for (i = 0; i < NUM_LINES * PG_SIZE; i++) {
-    a[i] = i % 17;
-  }
-
-
-  // Warmup
-  offset = 0;
-  for (j = 0; j < NUM_LINES; j++) {
-    offset += PG_SIZE + LINE_SIZE;
-    sum += a[offset];
-  }
-
-  RDTSC_START(tsc_before);
-  for (i = 0; i < RUNS; i++) {
-    offset = 0;
-    for (j = 0; j < NUM_LINES; j++) {
-      offset += PG_SIZE + LINE_SIZE;
+      offset += PG_SIZE + align;
       sum += a[offset];
     }
   }
@@ -80,7 +43,7 @@ int unaligned() {
 }
 
 int main() {
-  printf("Sum: %i\n", page_aligned());  
-  printf("Sum: %i\n", unaligned());  
+  printf("Sum: %i\n", access_mem(0));  
+  printf("Sum: %i\n", access_mem(LINE_SIZE));  
   return 0;
 }
