30f4af10d3670675d90cb16397ed6f1aafbfceca
Dan Luu <danluu@google.com>
2013-12-29 15:29:30 -0600

    Refactor redundant code

--- cache_conflict_benchmark.c
+++ cache_conflict_benchmark.c
@@ -1,19 +1,23 @@
-// Measure the cost of TLB thrashing from page-aligned accesses
+// Measure the cost of associativity misses
+
+#define __STDC_FORMAT_MACROS
 
 #include <stdio.h>
+#include <inttypes.h>
 #include "rdtsc.h"
 #include "stddev.h"
 
 #define RUNS 1000000
 
-#define NUM_LINES 1000
+#define MAX_NUM_LINES 10000
 #define LINE_SIZE 64
 #define PG_SIZE 4096
 
 inline uint64_t min(uint64_t a, uint64_t b) { return (a < b) ? a : b; }
 
-uint64_t access_mem(int align) {
-  static char a[2 * NUM_LINES * PG_SIZE];
+// Do 'n' accesses with a relative offset of 'align'
+uint64_t access_mem(int align, int n) {
+  static char a[2 * MAX_NUM_LINES * PG_SIZE];
   int sum = 0;
   uint64_t tsc_before, tsc_after, tsc, min_tsc;
   uint64_t offset;
@@ -22,13 +26,13 @@ uint64_t access_mem(int align) {
   min_tsc--;
 
   int i, j;
-  for (i = 0; i < NUM_LINES * PG_SIZE; i++) {
+  for (i = 0; i < n * PG_SIZE; i++) {
     a[i] = i % 17;
   }
 
   // Warmup
   offset = 0;
-  for (j = 0; j < NUM_LINES; j++) {
+  for (j = 0; j < n; j++) {
     offset += PG_SIZE + align;
     sum += a[offset];
   }
@@ -37,7 +41,7 @@ uint64_t access_mem(int align) {
   for (i = 0; i < RUNS; i++) {
     offset = 0;
     RDTSC_START(tsc_before);
-    for (j = 0; j < NUM_LINES; j++) {
+    for (j = 0; j < n; j++) {
       offset += PG_SIZE + align;
       sum += a[offset];
     }
@@ -52,25 +56,24 @@ uint64_t access_mem(int align) {
   return min_tsc;
 }
 
-int main() {
+void test_and_print(int n) {
   double diff;
   uint64_t aligned_time, unaligned_time;
 
-  aligned_time = access_mem(0);
-  unaligned_time = access_mem(LINE_SIZE);
+  aligned_time = access_mem(0, n);
+  unaligned_time = access_mem(LINE_SIZE, n);
   diff = (double)aligned_time / (double)unaligned_time;
 
-  printf("Page-aligned time:          %llu\n", aligned_time);
-  printf("Page-unaligned (+128) time: %llu\n", unaligned_time);
+  printf("----------%i accesses--------\n", n);
+  printf("Page-aligned time:         %" PRIu64 "\n", aligned_time);
+  printf("Page-unaligned (+64) time: %" PRIu64 "\n", unaligned_time);
   printf("Difference: %f\n", diff);
+}
 
-  aligned_time = access_mem(0);
-  unaligned_time = access_mem(LINE_SIZE);
-  diff = (double)aligned_time / (double)unaligned_time;
-
-  printf("Page-aligned time:          %llu\n", aligned_time);
-  printf("Page-unaligned (+128) time: %llu\n", unaligned_time);
-  printf("Difference: %f\n", diff);
+int main() {
+  test_and_print(10);
+  test_and_print(100);
+  test_and_print(1000);
 
   return 0;
 }
